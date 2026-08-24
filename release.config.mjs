daeab36ed49cec31030f9a683f5a3570edfd67a1
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2026-08-24 13:31:23 +0200

    fix(release): only release on gradle portal

--- release.config.mjs
+++ release.config.mjs
@@ -1,7 +1,5 @@
 const publishCmd = `
-./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication validateMavenCentralPortalPublication releaseMavenCentralPortalPublication || exit 1
 ./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2
-./gradlew publishAllPublicationsToGithubRepository || true
 `;
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 config.plugins.push(
